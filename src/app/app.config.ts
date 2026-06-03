ef2d060bbfc2f5bea6430da14cd4852593b6cdae
ByKan77 <sofiane.ouadah@gmail.com>
2026-06-03 17:37:26 +0200

    routing et configuration Apollo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { inject } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+      return {
+        link: httpLink.create({
+          uri: 'https://rickandmortyapi.com/graphql',
+        }),
+        cache: new InMemoryCache(),
+      };
+    }),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,71 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./pages/characters-list/characters-list.component').then(
+        (m) => m.CharactersListComponent
+      ),
+  },
+  {
+    path: 'characters/:id',
+    loadComponent: () =>
+      import('./pages/character-detail/character-detail.component').then(
+        (m) => m.CharacterDetailComponent
+      ),
+  },
+  {
+    path: 'locations',
+    loadComponent: () =>
+      import('./pages/locations-list/locations-list.component').then(
+        (m) => m.LocationsListComponent
+      ),
+  },
+  {
+    path: 'locations/:id',
+    loadComponent: () =>
+      import('./pages/location-detail/location-detail.component').then(
+        (m) => m.LocationDetailComponent
+      ),
+  },
+  {
+    path: 'episodes',
+    loadComponent: () =>
+      import('./pages/episodes-list/episodes-list.component').then(
+        (m) => m.EpisodesListComponent
+      ),
+  },
+  {
+    path: 'episodes/:id',
+    loadComponent: () =>
+      import('./pages/episode-detail/episode-detail.component').then(
+        (m) => m.EpisodeDetailComponent
+      ),
+  },
+  {
+    path: 'favoris',
+    loadComponent: () =>
+      import('./pages/favoris/favoris.component').then((m) => m.FavorisComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then((m) => m.ContactComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+];
